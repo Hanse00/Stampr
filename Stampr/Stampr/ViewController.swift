24b0abeee9572e111d17b5e6c8892da84d921ff0
Philip Mallegol-Hansen <philip@mallegolhansen.com>
2020-06-19 12:27:04 -0700

    Add NSFetchedResultsController to track the stamps

--- Stampr/Stampr/ViewController.swift
+++ Stampr/Stampr/ViewController.swift
@@ -11,20 +11,39 @@ import CoreData
 
 class ViewController: UIViewController {
     var container: NSPersistentContainer!
+    lazy var fetchedResultsController: NSFetchedResultsController<Stamp> = {
+        let controller = NSFetchedResultsController(fetchRequest: Stamp.sortedFetchRequest(), managedObjectContext: self.container.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        controller.delegate = self
+        return controller
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("Unable to perform fetch: \(error)")
+        }
+    }
     
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         if motion == .motionShake {
             let controller = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
             let addAction = UIAlertAction(title: "Add", style: .default) { (_) in
                 Stamp.add(to: self.container)
-                print(Stamp.getSortedStampts(from: self.container))
             }
             let printAction = UIAlertAction(title: "Print", style: .default) { (_) in
+                print("Stamps as seen by Core Data:")
                 print(Stamp.getSortedStampts(from: self.container))
+                print("Stamps as seen by NSFetchedResultsController:")
+                let stamps = self.fetchedResultsController.fetchedObjects!
+                for stamp in stamps {
+                    print("\(String(describing: self.fetchedResultsController.indexPath(forObject: stamp))): \(stamp)")
+                }
             }
             let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { (_) in
                 Stamp.deleteLatest(from: self.container)
-                print(Stamp.getSortedStampts(from: self.container))
             }
             let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
             controller.addAction(addAction)
@@ -38,3 +57,29 @@ class ViewController: UIViewController {
 
 }
 
+
+// MARK: - NSFetchedResultsControllerDelegate
+extension ViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        print("Going to change content")
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            print("Inserting \(anObject) to \(newIndexPath!)")
+        case .delete:
+            print("Deleting \(anObject) from \(indexPath!)")
+        case .update:
+            print("Updating \(anObject) at \(indexPath!)")
+        case .move:
+            print("Moving \(anObject) from \(indexPath!) to \(newIndexPath!)")
+        @unknown default:
+            fatalError("Some other option we weren't aware of")
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        print("Done changing content")
+    }
+}
