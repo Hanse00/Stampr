6f2f9612bb96eb180604a5a756fdbf904bb77aee
Philip Mallegol-Hansen <philip@mallegolhansen.com>
2020-06-19 10:51:55 -0700

    Convert stamp interface to UIAlertController

--- Stampr/Stampr/ViewController.swift
+++ Stampr/Stampr/ViewController.swift
@@ -12,36 +12,46 @@ import CoreData
 class ViewController: UIViewController {
     var container: NSPersistentContainer!
     
-    @IBAction func stampClicked(_ sender: UIButton) {
-        let stamp = Stamp(context: container.viewContext)
-        stamp.date = Date()
-        do {
-            try container.viewContext.save()
-        } catch {
-            fatalError("Unable to save new stamp: \(error)")
-        }
-        
-        printStamps()
-    }
-    
-    @IBAction func printClicked(_ sender: UIButton) {
-        printStamps()
-    }
-
-    @IBAction func deleteClicked(_ sender: UIButton) {
-        let stamps = getSortedStamps()
-        guard stamps.count >= 1 else {
-            print("Trying to delete from an empty state? Nice try.")
-            return
-        }
-        
-        let stamp = stamps[0]
-        do {
-            container.viewContext.delete(stamp)
-            try container.viewContext.save()
-            printStamps()
-        } catch {
-            fatalError("Unable to delete stamp: \(error)")
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            let controller = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            let addAction = UIAlertAction(title: "Add", style: .default) { (_) in
+                let stamp = Stamp(context: self.container.viewContext)
+                stamp.date = Date()
+                do {
+                    try self.container.viewContext.save()
+                } catch {
+                    fatalError("Unable to save new stamp: \(error)")
+                }
+                
+                self.printStamps()
+            }
+            let printAction = UIAlertAction(title: "Print", style: .default) { (_) in
+                self.printStamps()
+            }
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { (_) in
+                let stamps = self.getSortedStamps()
+                guard stamps.count >= 1 else {
+                    print("Trying to delete from an empty state? Nice try.")
+                    return
+                }
+                
+                let stamp = stamps[0]
+                do {
+                    self.container.viewContext.delete(stamp)
+                    try self.container.viewContext.save()
+                    self.printStamps()
+                } catch {
+                    fatalError("Unable to delete stamp: \(error)")
+                }
+            }
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            controller.addAction(addAction)
+            controller.addAction(printAction)
+            controller.addAction(deleteAction)
+            controller.addAction(cancelAction)
+            
+            present(controller, animated: true)
         }
     }
     
